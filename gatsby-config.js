3938f71d21d82c78b48a94793a31565ea0145ee7
Sebastien Delile <sebastiendelile@gmail.com>
2021-11-02 18:10:26 +0100

    finished part 4 of the tuto

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,17 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby official tuto",
+    pathPrefix: "/gatsby-official-tuto"
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
 };
